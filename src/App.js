9bc50f147e2c62a7ac2329f7d3432f283dfdbef2
Aasha Bhandari <bhandariaasha700@gmail.com>
2020-11-11 21:32:41 +0545

    updated styles

--- src/App.js
+++ src/App.js
@@ -1,46 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import deleteicon from "./trash-alt-solid.svg";
 
 function App() {
-
-  function changeText(e){
-    setCity(e.target.value)
-
+  function changeText(e) {
+    setCity(e.target.value);
   }
-  function checkWeather()
-  
-{
-  fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`)   //requesting server to give  response after
-  // we provide API and city
-    .then(response => response.json())
-    .then((response) => {
+  function checkWeather() {
+    fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`)
+      .then((response) => response.json())
+      .then((response) => {
         console.log(response);
-          setcityList([...cityList,response])
-    })
+        setcityList([...cityList, response]);
+        setCity("");
+      });
+  }
 
-}
-  const [city,setCity] = useState("")
-  const [cityList,setcityList] = useState([])
-  const API_KEY = "19212f7cf4574776bd961912201111"
+  function remove(index) {
+    let arr = cityList;
+    arr = arr.filter((item, index1) => {
+      return index1 !== index;
+    });
+    setcityList(arr);
+  }
+  const [city, setCity] = useState("");
+  const [cityList, setcityList] = useState([]);
+  const API_KEY = "19212f7cf4574776bd961912201111";
 
-
+  const colormapping = {
+    Fog: "fog",
+    Cloudy: "cloudy",
+    "Partly cloudy": "partly_cloudy",
+    Sunny: "sunny",
+    Mist: "mist",
+    Rain: "rainy",
+    Clear: "clear",
+    Overcast: "overcast",
+  };
 
   return (
     <div className="App">
-      <input type="text" onChange={(e)=>changeText(e)} value={city}></input>
-      <button onClick={checkWeather}>Go to Weather</button>
-      {cityList.map(item=>{
-        return (
-          <div className="desc">
-        <div>{item.current.temp_c}</div>
-        <div>{item.current.condition.text}</div>
-        <img src={item.current.condition.icon} className="img"/>
-        <div>{item.location.name}</div>
-        </div>
-        )
-      })}
+      <h1>CHECK WEATHER</h1>
+      <input
+        type="text"
+        onChange={(e) => changeText(e)}
+        value={city}
+        className="input_box"
+        placeholder="Enter city name"
+      ></input>
+      <button onClick={checkWeather} className="btn_click">
+        Go to Weather
+      </button>
+      <div className="container">
+        {cityList.map((item, index) => {
+          return (
+            <div
+              className={"desc " + colormapping[item.current.condition.text]}
+            >
+              <div className="temp">{item.current.temp_c} &#8451;</div>
+              <div>{item.current.condition.text}</div>
+              <img src={item.current.condition.icon} className="img" />
+              <div>{item.location.name}</div>
+              <img
+                src={deleteicon}
+                onClick={(e) => remove(index)}
+                className="btn_remove"
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
