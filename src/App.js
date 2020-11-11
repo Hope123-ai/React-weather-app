751417b50cc2e2668e05baf34b0f59e1ae6650f8
Aasha Bhandari <bhandariaasha700@gmail.com>
2020-11-11 14:32:34 +0545

    added weatherAPI & styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  function changeText(e){
+    setCity(e.target.value)
+
+  }
+  function checkWeather()
+  
+{
+  fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`)   //requesting server to give  response after
+  // we provide API and city
+    .then(response => response.json())
+    .then((response) => {
+        console.log(response);
+          setcityList([...cityList,response])
+    })
+
+}
+  const [city,setCity] = useState("")
+  const [cityList,setcityList] = useState([])
+  const API_KEY = "19212f7cf4574776bd961912201111"
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={(e)=>changeText(e)} value={city}></input>
+      <button onClick={checkWeather}>Go to Weather</button>
+      {cityList.map(item=>{
+        return (
+          <div className="desc">
+        <div>{item.current.temp_c}</div>
+        <div>{item.current.condition.text}</div>
+        <img src={item.current.condition.icon} className="img"/>
+        <div>{item.location.name}</div>
+        </div>
+        )
+      })}
     </div>
   );
 }
